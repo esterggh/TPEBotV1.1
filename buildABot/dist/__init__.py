42f8fb71f8baa12187d9b813687ea02f3ae005ac
EsterG <egohgh@gmail.com>
2023-03-24 04:34:38 +0800

    updated init

--- buildABot/dist/__init__.py
+++ buildABot/dist/__init__.py
@@ -1,9 +1,11 @@
-from buildABot.Manager import Manager
-from buildABot.Database import Database
-from buildABot.Entities import Entities
-from buildABot.HotTopics import HotTopics
-from buildABot.Intents import Intents
-from buildABot.WebApp import WebApp
-#from buildABot.Webhook import Webhook
+from buildABot.dist.Manager import Manager
+from buildABot.dist.Database import Database
+from buildABot.dist.Entities import Entities
+from buildABot.dist.HotTopics import HotTopics
+from buildABot.dist.Intents import Intents
+from buildABot.dist.WebApp import WebApp
+from buildABot.dist.Webhook import Webhook
+from buildABot.dist.Paraphraser import Paraphraser
+
 
 
